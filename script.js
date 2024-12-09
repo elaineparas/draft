36189e7316ee219265ca5431b02b0d72b1b09712
len <elainelobete.paras24@bicol-u.edu.ph>
2024-12-09 21:58:11 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+window.onscroll = function() {
+            var button = document.querySelector('.back-to-top');
+            if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+                button.style.display = "block";
+            } else {
+                button.style.display = "none";
+            }
+        };
