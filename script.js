1aabcc781ee89f2101eb26c6ae172a0a68515df5
len <elainelobete.paras24@bicol-u.edu.ph>
2024-12-09 22:01:08 +0800

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-window.onscroll = function() {
-            var button = document.querySelector('.back-to-top');
-            if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-                button.style.display = "block";
-            } else {
-                button.style.display = "none";
-            }
-        };
